e9e02421f69c9a18d1fd99867a6cf1284cd2add4
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2025-01-21 10:27:17 +0900

    first commit

---
+++ src/content.config.ts
@@ -0,0 +1,11 @@
+import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { blog };
