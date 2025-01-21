912be90c77bb6476331d2a27bbd2df252f944c54
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2025-01-21 14:59:46 +0900

    add Github Actions workiflow for deployment & tweak astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import remarkImageOptimization from "./src/lib/remark-image-optimization";
@@ -7,6 +7,9 @@ import remarkImageOptimization from "./src/lib/remark-image-optimization";
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkImageOptimization],
