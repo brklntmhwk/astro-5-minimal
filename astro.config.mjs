dce1399d69d038253b17c125bad14ca0176e96ce
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2025-01-23 13:09:33 +0900

    comment out cf adapter in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig /* passthroughImageService */ } from "astro/config";
+// import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import remarkImageOptimization from "./src/lib/remark-image-optimization";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  image: {
-    service: passthroughImageService(),
-  },
+  // adapter: cloudflare(),
+  // image: {
+  //   service: passthroughImageService(),
+  // },
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkImageOptimization],
